a34eb6b3099ded85a5be596bab9611169799ad34
Abhishek <ch89abhi@gmail.com>
2026-03-29 11:04:29 +0530

    initiated the spring boot project

---
+++ src/main/java/com/example/fakecom/FakecomApplication.java
@@ -0,0 +1,13 @@
+package com.example.fakecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakecomApplication.class, args);
+	}
+
+}
